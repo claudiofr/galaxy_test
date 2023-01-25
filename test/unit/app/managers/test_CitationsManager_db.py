5a58113e955a5fd56496740ccde8f5193557bc4a
claudiofr <claudiofr@yahoo.com>
2023-01-25 14:49:36 -0500

    Fix testing errors after uploading changes to support a database based beaker cache.
    Use a sqlite in memory database for unit testing.

--- test/unit/app/managers/test_CitationsManager_db.py
+++ test/unit/app/managers/test_CitationsManager_db.py
@@ -1,11 +1,27 @@
 import pytest
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
 
 from galaxy.managers.citations import DoiCache
 from galaxy import config
 
+
+class TestDoiCache(DoiCache):
+    def __init__(self, config):
+        cache_opts = {
+            "cache.type": getattr(config, "citation_cache_type", "ext:database"),
+            "cache.data_dir": getattr(config, "citation_cache_data_dir", None),
+            "cache.lock_dir": getattr(config, "citation_cache_lock_dir", None),
+            "cache.url": "sqlite://",
+            "cache.table_name": getattr(config, "citation_cache_table_name", "citation_beaker_cache"),
+            "cache.schema_name": getattr(config, "citation_cache_schema_name", None),
+        }
+        self._cache = CacheManager(**parse_cache_config_options(cache_opts)).get_cache("doi")
+
 @pytest.fixture()
 def doi_cache():
-    return DoiCache(config.GalaxyAppConfiguration(override_tempdir=False))
+    return TestDoiCache(config.GalaxyAppConfiguration(override_tempdir=False))
+
 
 def test_DoiCache(doi_cache):
     assert "Jörg" in doi_cache.get_bibtex("10.1093/bioinformatics/bts252")
